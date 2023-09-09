d0d43a29a37801a949083c576474c3606e483dea
league <league@imac-34.attlocal.net>
2023-09-08 18:27:15 -0700

    whack whack whack

--- src/_09_whack_a_mole/WhackAMole.java
+++ src/_09_whack_a_mole/WhackAMole.java
@@ -1,58 +1,118 @@
 package _09_whack_a_mole;
 
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
 	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+	JPanel panel;
 	JButton specialButton;
-	
+	int MolesWhacked = 0;
+	int MolesMissed = 0;
+	Date TimeStarted;
+
 	void setup() {
 		frame.setVisible(true);
-		frame.add(panel);
 		frame.setTitle("Whack-A-Mole");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		TimeStarted = new Date();
 	}
-	
-void drawButtons(int r) {
-	for(int i=0; i < 24; i++) {
-		JButton button = new JButton();
-		panel.add(button);
-		button.addActionListener(this);
-		if (i == r) {
-			button.setText("Mole!");
-			specialButton = button;
+
+	void drawButtons(int r) {
+		panel = new JPanel();
+		frame.add(panel);
+		for(int i=0; i < 24; i++) {
+			JButton button = new JButton();
+			panel.add(button);
+			button.addActionListener(this);
+			if (i == r) {
+				button.setText("Mole!");
+				specialButton = button;
+			}
+			frame.setSize(300, 350);}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() != specialButton) {
+			MolesMissed++;
+			if (MolesMissed == 1) {
+				speak("Missed!");
+			}
+			if (MolesMissed == 2) {
+				speak("Try again!");
+			}
+			if (MolesMissed == 3) {
+				speak("Wrong mole!");
+			}
+			if (MolesMissed == 4) {
+				speak("Ouch!");
+			}
+			frame.remove(panel);
+			drawButtons(new Random().nextInt(24));
+			frame.add(panel);
+			frame.pack();
+			frame.setSize(300, 350);
+			if (MolesMissed == 5) {
+				endGame(TimeStarted, MolesWhacked);
+				speak("Please try harder next time!");
+			}
 		}
-frame.setSize(300, 350);}
+		else {
+			MolesWhacked++;
+			playSound("whack.wav");
+
+			frame.remove(panel);
+			drawButtons(new Random().nextInt(24));
+			frame.add(panel);
+			frame.pack();
+			frame.setSize(300, 350);
+
+			if (MolesWhacked == 10) {
+				endGame(TimeStarted, MolesWhacked);
+				speak("Great Job!");
+			}
+		}
+	}
+
+	static void speak(String words) {
+		if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+			String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+					+ words + "');\"";
+			try {
+				Runtime.getRuntime().exec( cmd ).waitFor();
+			} catch( Exception e ) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				Runtime.getRuntime().exec( "say " + words ).waitFor();
+			} catch( Exception e ) {
+				e.printStackTrace();
+			}
+		} 
+	}
+
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+				+ " moles per second.");
+	}
+
+	private void playSound(String fileName) { 
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
 }
 
-@Override
-public void actionPerformed(ActionEvent e) {
-if (e.getSource() != specialButton) {
-	   static void speak(String words) {
-	        if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
-	            String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
-	                    + words + "');\"";
-	            try {
-	                Runtime.getRuntime().exec( cmd ).waitFor();
-	            } catch( Exception e ) {
-	                e.printStackTrace();
-	            }
-	        } else {
-	            try {
-	                Runtime.getRuntime().exec( "say " + words ).waitFor();
-	            } catch( Exception e ) {
-	                e.printStackTrace();
-	            }
-	        }
-	        }
-	    }
-else {
-	
-}
---
+++ src/_10_slot_machine/SlotMachine.java
@@ -0,0 +1,103 @@
+package _10_slot_machine;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class SlotMachine implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JLabel label1;
+	JLabel label2;
+	JLabel label3;
+	String randomFruit;
+	String label1Fruit;
+	String label2Fruit;
+	String label3Fruit;
+	
+	void setup() {
+		try {
+			label1 = createLabelImage("slotOrange.png");
+			label2 = createLabelImage("slotLemon.png");
+			label3 = createLabelImage("slotApple.jpeg");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(label1);
+		panel.add(label2);
+		panel.add(label3);
+		panel.add(button);
+
+		button.setText("SPIN!");
+		button.addActionListener(this);
+
+		frame.pack();
+	}
+
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null){
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		frame.remove(panel);
+		
+		try {
+			setRandomFruit();
+			label1 = createLabelImage("slot" + randomFruit);
+			label1Fruit = randomFruit;
+			
+			setRandomFruit();
+			label2 = createLabelImage("slot" + randomFruit);
+			label2Fruit = randomFruit;
+	
+			setRandomFruit();
+			label3 = createLabelImage("slot" + randomFruit);
+			label3Fruit = randomFruit;
+		} catch (MalformedURLException o) {
+			o.printStackTrace();
+		
+	}
+	
+		if (label1Fruit == label2Fruit && label2Fruit == label3Fruit) {
+				JOptionPane.showMessageDialog(null, "YOU WON!");
+		}
+		frame.add(panel);
+		frame.pack();
+}
+	
+	void setRandomFruit() {
+		int r = new Random().nextInt(3);
+		if (r == 0) {
+			randomFruit = "Orange.png";
+		}
+		else if (r == 1) {
+			randomFruit = "Lemon.png";
+		}
+		else {
+			randomFruit = "Apple.jpeg";
+		}
+	}
+}
